8997941307b4d0f6a104d73eeea5813bdd723c5a
jang0710 <janga9710@naver.com>
2023-08-28 23:54:29 +0900

    전화걸기 (미완)

--- List_App/app/src/main/java/com/example/listapp/CallItem.kt
+++ List_App/app/src/main/java/com/example/listapp/CallItem.kt
@@ -7,7 +7,7 @@ data class CallItem(
     val viewType: Int
 ) {
     companion object {
-        // 꼭 const쓰지 않아도 된다. java에서는 구분을 해주어야 하지만 kotlin에서는 알아서 값을 가져 온다.
+        // 꼭 const쓰지 않아도 된다. java에서는 구분을 해주어야 하지만 kotlin에서는 알아서 핟당한다. 차이점은 발생하는 오버헤드(프로그램이 실행되는 중에 다른 위치의 코드를 실행시켜야 할 때, 간접적으로 시간, 메모리, 자원 등이 사용되는 현상이다.)를 줄일수 있다는 점이다.
         const val VIEW_TYPE_LEFT = 0
         const val VIEW_TYPE_RIGHT = 1
     }

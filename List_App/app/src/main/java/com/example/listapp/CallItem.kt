6dfaaf076625433526ec0fde38d2b7d076c206dd
jang0710 <janga9710@naver.com>
2023-08-28 23:47:48 +0900

    전화걸기 (미완)

--- List_App/app/src/main/java/com/example/listapp/CallItem.kt
+++ List_App/app/src/main/java/com/example/listapp/CallItem.kt
@@ -2,7 +2,8 @@ package com.example.listapp
 
 data class CallItem(val aIcon:Int, val aName:String, val aPhoneNumber:String, val viewType: Int) {
     companion object {
-        const val VIEW_TYPE_LEFT = 0
-        const val VIEW_TYPE_RIGHT = 1
+        // 꼭 const쓰지 않아도 된다. java에서는 구분을 해주어야 하지만 kotlin에서는 알아서 값을 가져 온다.
+         val VIEW_TYPE_LEFT = 0
+         val VIEW_TYPE_RIGHT = 1
     }
 }

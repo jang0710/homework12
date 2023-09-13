96f9ae08fba84a5530438d9f4efca81f344fe4bc
jang0710 <janga9710@naver.com>
2023-09-13 16:09:54 +0900

    계산기 Lv3

--- new_Calculator/app/src/main/java/com/example/new_calculator/main.kt
+++ new_Calculator/app/src/main/java/com/example/new_calculator/main.kt
@@ -2,12 +2,11 @@ package com.example.new_calculator
 
 fun main() {
 
-    println("숫자를 입력하세요.")
-    var a = readLine()!!.toInt()
-    println("부호를 입력하세요.")
-    var op = readLine()!!.toString()
-    println("다음 숫자를 입력하세요.")
-    var b = readLine()!!.toInt()
+    var a = inputMyInfo("a").toString().toInt()
+
+    var op = inputMyInfo("c").toString()
+
+    var b = inputMyInfo("b").toString().toInt()
 
     val calc = Calculator()
     if (op == "+") {
@@ -20,5 +19,49 @@ fun main() {
         println("계산 결과는 ${calc.DivideOperation(a,b)}입니다.")
     } else {
         println("잘못된 계산입니다.")
+    }
+}
+fun inputMyInfo(type: String): Any? {
+    return when (type) {
+        "a" -> {
+            println("숫자를 입력해 주세요.")
+            while (true) {
+                try {
+                    var originA = readLine()
+                    return originA?.toInt() ?: -1
+                } catch (e: Exception) { // 모든 오류를 잡아주고 다시 입력창으로 돌려줌.
+                    println("숫자를 다시 입력해 주세요.")
+                }
+            }
+        }
+        "c" -> {
+            println("사칙연산을 입력해 주세요.")
+            while (true) {
+                try {
+                    var originC = readLine()
+                    if (originC?.equals("+") == true || originC?.equals("-") == true || originC?.equals("*") == true || originC?.equals("/") == true) {
+                        return originC
+                    } else {
+                        println("다시 입력해 주세요. ex:( +, -, *, /)")
+                    }
+                } catch (e: Exception) {
+                    println("다시 입력해 주세요. ex:( +, -, *, /)")
+                }
+            }
+        }
+        "b" -> {
+            println("다음 숫자를 입력해 주세요.")
+            while (true) {
+                try {
+                    var originB = readLine()
+                    return originB?.toInt() ?: -1
+                } catch (e: Exception) {
+                    println("숫자를 다시 입력해 주세요.")
+                }
+            }
+        }
+        else -> {
+            return "no"
+        }
     }
 }
